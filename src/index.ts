7d82b09704549af0ec92114e20b9a12cbe7744f5
tomoyuki65 <tomoyuki65@users.noreply.github.com>
2024-10-19 18:32:19 +0900

    init

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,37 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+// 環境変数の定義
+interface Env {
+    ENV: string;
+}
+
+async function wait(seconds: number): Promise<void> {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve();
+      }, seconds * 1000);
+    });
+}
+
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
+    async fetch(request, env, ctx): Promise<Response> {
+        return new Response('Hello World!');
+    },
+    // トリガー起動用の処理を追加
+    async scheduled(
+        controller: ScheduledController,
+        env: Env,
+        ctx: ExecutionContext,
+    ) {
+        console.log("トリガー起動により実行されました！！");
+        console.log(`ENV: ${env.ENV}`);
+
+        await wait(10);
+        console.log("10秒経過");
+
+        await wait(10);
+        console.log("さらに10秒経過。完了です。");
+
+    },
 } satisfies ExportedHandler<Env>;
